ab8917cd77360d3f6f197ea60c14bdd2a567c7ad
Jeremy Walton <jthelegoman@gmail.com>
2019-11-30 13:32:00 -0500

    Display lists

--- src/App.js
+++ src/App.js
@@ -1,16 +1,62 @@
 import React from 'react';
 import './assets/css/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <div>Christmas Wishlists</div>
-        <a href='https://www.launchtabs.com/' className='home-link'>Other Cool Stuff</a>
-      </header>
-      <p>Things on a page</p>
-    </div>
-  );
-}
+import List from  './models/List';
 
-export default App;
+export default class App extends React.Component {
+  constructor() {
+    super();
+
+    const data = localStorage.state ? JSON.parse(localStorage.state) : { lists: [] };
+    let lists = data.lists.map(list => new List(list));
+
+    this.state = { term: '', lists };
+  }
+
+  componentDidUpdate() {
+    localStorage.state = JSON.stringify(this.state);
+  }
+
+  onChange = (event) => {
+    this.setState({ term: event.target.value });
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault()
+
+    const { lists, term } = this.state
+    const newList = new List({ term })
+
+    this.setState({
+      term: '',
+      lists: [...lists, newList]
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <div>Christmas Wishlists</div>
+          <a href='https://www.launchtabs.com/' className='home-link'>Other Cool Stuff</a>
+        </header>
+        <div className='header-fix'></div>
+
+        <div className="container">
+          <div className="wishlist-container">
+            {
+              this.state.lists.map((item, index) => <div className="wishlist" key={index}>{item.term}</div>)
+            }
+          </div>
+
+          <form className="" onSubmit={this.onSubmit}>
+            <input value={this.state.term} onChange={this.onChange} />
+            <button>Submit</button>
+          </form>
+        </div>
+
+        <button onClick={() => { this.setState({ lists: [] }) }}>Clear Data</button>
+      </div>
+    );
+  }
+};
